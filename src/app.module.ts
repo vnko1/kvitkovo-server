c217d7bd81cb01ca92e26358e72330bfb49c645e
Andrii Valenko <andreyvalenko@gmail.com>
2024-07-19 09:35:14 +0300

    update db conf

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,17 +10,16 @@ import { User } from './modules/user/models';
     ConfigModule.forRoot({ isGlobal: true }),
     SequelizeModule.forRootAsync({
       useFactory: () => ({
-        uri: process.env.DB_URL,
+        dialect: 'mysql',
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
         retryAttempts: 2,
         synchronize: true,
         autoLoadModels: true,
         models: [User],
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        },
       }),
     }),
     UserModule,
