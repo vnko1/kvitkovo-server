f8be720869745acd035e3e0e2225f3c98b492296
Andrii Valenko <andreyvalenko@gmail.com>
2024-07-23 13:37:30 +0300

    save updates

--- src/modules/catalog/models/color.model.ts
+++ src/modules/catalog/models/color.model.ts
@@ -1,4 +1,27 @@
-import { Model, Table } from "sequelize-typescript";
+import {
+  AutoIncrement,
+  BelongsTo,
+  Column,
+  DataType,
+  Model,
+  PrimaryKey,
+  Table,
+} from "sequelize-typescript";
+import { Product } from "./product.model";
 
 @Table
-export class Color extends Model {}
+export class Color extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column({ type: DataType.INTEGER })
+  colorId: number;
+
+  @Column({ type: DataType.STRING })
+  name: string;
+
+  @Column({ type: DataType.STRING })
+  alias: string;
+
+  @BelongsTo(() => Product)
+  product: Product;
+}
