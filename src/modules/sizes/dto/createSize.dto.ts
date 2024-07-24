55b56e4fb07685efb731b34262d34310a461b2ba
Andrii Valenko <andreyvalenko@gmail.com>
2024-07-24 15:43:02 +0300

    update dto

--- src/modules/sizes/dto/createSize.dto.ts
+++ src/modules/sizes/dto/createSize.dto.ts
@@ -3,6 +3,9 @@ import { z } from "zod";
 export const createSizeSchema = z
   .object({
     name: z.string({ required_error: "Name is required" }),
+    alias: z.string({ required_error: "Alias is required" }),
+    min: z.number({ required_error: "Min is required" }),
+    max: z.number({ required_error: "Max is required" }),
   })
   .required();
 
